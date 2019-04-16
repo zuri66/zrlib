ec4d43c1f5c2f649110e947d9d3be94c2e6c46e6
zuri <webzuri@gmail.com>
2019-04-17 00:25:39 +0200

    Vector2SideStrategy, code-->

--- src/base/Vector/Vector2SideStrategy.c
+++ src/base/Vector/Vector2SideStrategy.c
@@ -4,11 +4,79 @@
  */
 
 #include <zrlib/base/Vector/Vector2SideStrategy.h>
-
+#include <zrlib/base/Allocator/Allocator.h>
 #include <assert.h>
 #include <stdbool.h>
 
 #define INITIAL_SIZE 64*3
+
+typedef struct ZRVector_2SideDataS ZRVector_2SideData;
+
+// ============================================================================
+
+struct ZRVector_2SideDataS
+{
+	/*
+	 * Memory segment, or unused first memory space
+	 */
+	void * restrict FSpace;
+
+	/*
+	 * Effectively used memory space
+	 * Replaced by the array field of ZRVector
+	 */
+	// void * restrict USpace;
+	/*
+	 * Unused end memory space
+	 */
+	void * restrict ESpace;
+
+	/*
+	 * First address of outside memory segment
+	 */
+	void * restrict OSpace;
+};
+
+// ============================================================================
+
+#define ZRVECTOR_DATA(V) ((ZRVector_2SideData*)((V)->sdata))
+#define ZRVECTOR_STRATEGY(V) ((ZRVector_2SideStrategy*)((V)->strategy))
+
+#define ZRVECTOR_FSPACE(V) ZRVECTOR_DATA(V)->FSpace
+#define ZRVECTOR_USPACE(V) (V)->array
+#define ZRVECTOR_ESPACE(V) ZRVECTOR_DATA(V)->ESpace
+#define ZRVECTOR_OSPACE(V) ZRVECTOR_DATA(V)->OSpace
+
+#define ZRVECTOR_FSPACE_SIZEOF(V) (ZRVECTOR_USPACE(V) - ZRVECTOR_FSPACE(V))
+#define ZRVECTOR_USPACE_SIZEOF(V) (ZRVECTOR_ESPACE(V) - ZRVECTOR_USPACE(V))
+#define ZRVECTOR_ESPACE_SIZEOF(V) (ZRVECTOR_OSPACE(V) - ZRVECTOR_ESPACE(V))
+#define ZRVECTOR_TOTALSPACE_SIZEOF(V)  (ZRVECTOR_OSPACE(V) - ZRVECTOR_FSPACE(V))
+#define ZRVECTOR_FREESPACE_SIZEOF(V) (ZRVECTOR_FSPACE_SIZEOF(V) + ZRVECTOR_ESPACE_SIZEOF(V))
+
+// ============================================================================
+
+ZRVector_2SideStrategy ZRVector_2SideStrategy_object(ZRAllocator *allocator)
+{
+	ZRVector_2SideStrategy ret = { //
+		.strategy = { //
+			.fdataSize = ZRVector_2SideStrategy_dataSize, //
+			.finsert = ZRVector_2SideStrategy_insert, //
+			.fdelete = ZRVector_2SideStrategy_delete //
+			},//
+		.allocator = allocator  //
+		};
+	return ret;
+}
+
+void ZRVector_2SideStrategy_init(ZRVector_2SideStrategy *strategy, ZRAllocator *allocator)
+{
+	*strategy = ZRVector_2SideStrategy_object(allocator);
+}
+
+size_t ZRVector_2SideStrategy_dataSize(void)
+{
+	return sizeof(ZRVector_2SideData);
+}
 
 static inline void firstAlloc(ZRVector *vec)
 {
@@ -16,18 +84,18 @@ static inline void firstAlloc(ZRVector *vec)
 	size_t const objSize = vec->objSize;
 	size_t const nbBytes = objSize * initialSize;
 
-	vec->FSpace = vec->allocator->alloc(nbBytes);
-	vec->USpace = (char*)vec->FSpace + nbBytes / 3;
-	vec->ESpace = (char*)vec->USpace + objSize;
-	vec->OSpace = (char*)vec->FSpace + nbBytes;
+	ZRVECTOR_FSPACE(vec) = ZRALLOC(ZRVECTOR_STRATEGY(vec)->allocator, nbBytes);
+	ZRVECTOR_USPACE(vec) = (char*)ZRVECTOR_FSPACE(vec) + (nbBytes / 3);
+	ZRVECTOR_ESPACE(vec) = (char*)ZRVECTOR_USPACE(vec) + objSize;
+	ZRVECTOR_OSPACE(vec) = (char*)ZRVECTOR_FSPACE(vec) + nbBytes;
 }
 
 static inline void moreSize(ZRVector *vec)
 {
 	size_t const nbBytes = ZRVECTOR_TOTALSPACE_SIZEOF(vec) << 1;
 
-	vec->FSpace = vec->allocator->realloc(vec->FSpace, nbBytes);
-	vec->OSpace = (char*)vec->FSpace + nbBytes;
+	ZRVECTOR_FSPACE(vec) = ZRREALLOC(ZRVECTOR_STRATEGY(vec)->allocator, ZRVECTOR_DATA(vec)->FSpace, nbBytes);
+	ZRVECTOR_OSPACE(vec) = (char*)ZRVECTOR_FSPACE(vec) + nbBytes;
 }
 
 static bool mustResize(ZRVector *vec)
@@ -39,7 +107,7 @@ static bool mustResize(ZRVector *vec)
 
 void ZRVector_2SideStrategy_insert(ZRVector *vec, size_t pos)
 {
-	if (vec->FSpace == NULL)
+	if (ZRVECTOR_FSPACE(vec) == NULL)
 	{
 		firstAlloc(vec);
 		return;
@@ -50,31 +118,31 @@ void ZRVector_2SideStrategy_insert(ZRVector *vec, size_t pos)
 
 	if (toTheLeft)
 	{
-		if (vec->FSpace == vec->USpace)
+		if (ZRVECTOR_FSPACE(vec) == ZRVECTOR_USPACE(vec))
 		{
 			if (mustResize(vec))
 				moreSize(vec);
 
-			ZRMemoryOp_shift(vec->FSpace, vec->OSpace, shiftBytes, true);
-			vec->FSpace = (char*)vec->FSpace + shiftBytes;
-			vec->USpace = (char*)vec->USpace + shiftBytes;
+			ZRMemoryOp_shift(ZRVECTOR_FSPACE(vec), ZRVECTOR_OSPACE(vec), shiftBytes, true);
+			ZRVECTOR_FSPACE(vec) = (char*)ZRVECTOR_FSPACE(vec) + shiftBytes;
+			ZRVECTOR_USPACE(vec) = (char*)ZRVECTOR_USPACE(vec) + shiftBytes;
 		}
 	}
 	else
 	{
-		if (vec->ESpace == vec->USpace + vec->nbObj)
+		if (ZRVECTOR_ESPACE(vec) == ZRVECTOR_USPACE(vec) + vec->nbObj)
 		{
 			if (mustResize(vec))
 				moreSize(vec);
 
-			ZRMemoryOp_shift(vec->FSpace, vec->OSpace, shiftBytes, false);
-			vec->FSpace = (char*)vec->FSpace - shiftBytes;
-			vec->USpace = (char*)vec->USpace - shiftBytes;
+			ZRMemoryOp_shift(ZRVECTOR_FSPACE(vec), ZRVECTOR_OSPACE(vec), shiftBytes, false);
+			ZRVECTOR_FSPACE(vec) = (char*)ZRVECTOR_FSPACE(vec) - shiftBytes;
+			ZRVECTOR_USPACE(vec) = (char*)ZRVECTOR_USPACE(vec) - shiftBytes;
 		}
 	}
 }
 
 void ZRVector_2SideStrategy_delete(ZRVector *vec, size_t pos)
 {
-	assert(vec->FSpace != NULL);
+	assert(ZRVECTOR_FSPACE(vec) != NULL);
 }
--- zrlib/base/Vector/Vector2SideStrategy.h
+++ zrlib/base/Vector/Vector2SideStrategy.h
@@ -3,21 +3,34 @@
  * @date samedi 19 janvier 2019, 18:03:00 (UTC+0100)
  */
 
-#ifndef VECTOR2SIDESTRATEGY
-#define VECTOR2SIDESTRATEGY
+#ifndef VECTOR2SIDESTRATEGY_H
+#define VECTOR2SIDESTRATEGY_H
 
 #include <zrlib/config.h>
-#include "./Vector.h"
-
-typedef struct
-{
-	ZRVectorStrategy strategy;
-} ZRVector_2SideStrategy;
+#include <zrlib/base/Allocator/Allocator.h>
+#include <zrlib/base/Vector/Vector.h>
 
 // ============================================================================
 
-ZRVector_2SideStrategy ZRVector_2SideStrategy_init();
+typedef struct ZRVector_2SideStrategyS ZRVector_2SideStrategy;
 
+// ============================================================================
+
+struct ZRVector_2SideStrategyS{
+	ZRVectorStrategy strategy;
+
+	/*
+	 * Allocator for the vector's array.
+	 */
+	ZRAllocator *allocator;
+};
+
+// ============================================================================
+
+ZRVector_2SideStrategy ZRVector_2SideStrategy_object(ZRAllocator *allocator);
+void ZRVector_2SideStrategy_init(ZRVector_2SideStrategy *strategy, ZRAllocator *allocator);
+
+size_t ZRVector_2SideStrategy_dataSize(void);
 void ZRVector_2SideStrategy_insert(ZRVector *vec, size_t pos);
 void ZRVector_2SideStrategy_delete(ZRVector *vec, size_t pos);
 
