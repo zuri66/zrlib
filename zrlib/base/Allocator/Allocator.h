1a3b983e3b07825bb55527dc95ed76f71ed1f66d
zuri <webzuri@gmail.com>
2019-04-17 00:20:43 +0200

    Allocator.h, code-->

--- zrlib/base/Allocator/Allocator.h
+++ zrlib/base/Allocator/Allocator.h
@@ -3,9 +3,29 @@
  * @date dimanche 20 janvier 2019, 18:08:52 (UTC+0100)
  */
 
-typedef struct
+#ifndef ALLOCATOR_H
+#define ALLOCATOR_H
+
+#include <zrlib/config.h>
+
+#include <stddef.h>
+#include <zrlib/syntax_pad.h>
+
+// ============================================================================
+
+typedef struct ZRAllocatorS ZRAllocator;
+
+struct ZRAllocatorS
 {
-	void* (*alloc)(size_t nbBytes);
-	void* (*realloc)(void *allocated, size_t nbBytes);
-	void* (*free)(void *allocated);
-} ZRAllocator;
+	void* (*falloc)__(ZRAllocator *allocator, size_t nbBytes);
+	void* (*frealloc)(ZRAllocator *allocator, void *allocated, size_t nbBytes);
+	void _(*ffree)___(ZRAllocator *allocator, void *allocated);
+};
+
+// ============================================================================
+
+#define ZRALLOC(allocator,nbBytes) (allocator)->falloc(allocator,nbBytes)
+#define ZRREALLOC(allocator,allocated,nbBytes) (allocator)->frealloc(allocator,allocated,nbBytes)
+#define ZRFREE(allocator,allocated) (allocator)->ffree(allocator,allocated)
+
+#endif
