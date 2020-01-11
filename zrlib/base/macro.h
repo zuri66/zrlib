728f7c738493649e7b59bbb6992c591bcceffe82
zuri <webzuri@gmail.com>
2020-01-12 00:49:19 +0100

    macro.h, add ZRSTRUCTSIZE_FAM_PAD

--- zrlib/base/macro.h
+++ zrlib/base/macro.h
@@ -3,6 +3,9 @@
  * @date mardi 18 décembre 2018, 22:46:00 (UTC+0100)
  */
 
+#ifndef ZRMACRO_H
+#define ZRMACRO_H
+
 #define ZRSIZE_UNKNOW (~(size_t)0)
 #define ZRTOSTRING(V) #V
 
@@ -12,3 +15,21 @@
 #define ZRMIN(a,b) ((a) < (b) ? (a) : (b))
 
 #define ZRCARRAY_NBOBJ(array) (sizeof(array)/sizeof(*array))
+
+//#define ZRSTRUCT_FAM
+
+/**
+ * Size for structure with a flexible array member which conserve the structure alignment.
+ * The size of the FAM must be counted in the structSize argument.
+ */
+static inline size_t ZRSTRUCTSIZE_FAM_PAD(size_t structSize, size_t alignment)
+{
+	size_t const rest = structSize % alignment;
+
+	if(rest)
+		return structSize + alignment - rest;
+
+	return structSize;
+}
+
+#endif
