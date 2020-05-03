2d843e2e0ada8b341abd2e9f326afa7e30102fd5
zuri <webzuri@gmail.com>
2020-05-03 13:14:16 +0200

    macro.h, add ZRMACRO_NARGS

--- zrlib/base/macro.h
+++ zrlib/base/macro.h
@@ -14,14 +14,29 @@
 #define ZRCONCAT(A,B) ZRCONCAT_(A,B)
 #define ZRCONCAT_(A,B) A ## B
 
-#define ZRMIN(a,b) ((a) < (b) ? (a) : (b))
-#define ZRMAX(a,b) ((a) > (b) ? (a) : (b))
-#define ZRMAX_3(a,b,c) ZRMAX(a,ZRMAX(b,c))
+#define ZRMACRO_NARGS(...) ZRMACRO_NARGS_(__VA_ARGS__, ZRMACRO_REVERSE_NARGS())
+#define ZRMACRO_NARGS_(...) ZRMACRO_NARGS__(__VA_ARGS__)
+#define ZRMACRO_NARGS__(_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14,_15,N,_...) N
+#define ZRMACRO_REVERSE_NARGS() 15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0
+
+
+#define ZRMAX(...) ZRCONCAT(ZRMAX_,ZRMACRO_NARGS(__VA_ARGS__))(__VA_ARGS__)
+#define ZRMIN(...) ZRCONCAT(ZRMIN_,ZRMACRO_NARGS(__VA_ARGS__))(__VA_ARGS__)
+
+
+#define ZRMIN_2(a,b) ((a) < (b) ? (a) : (b))
+#define ZRMIN_3(a,b,c) ZRMIN_2(a,ZRMIN_2(b,c))
+#define ZRMIN_4(a,b,c,d) ZRMIN_2(ZRMIN_2(a,b),ZRMIN_2(c,d))
+
+#define ZRMAX_2(a,b) ((a) > (b) ? (a) : (b))
+#define ZRMAX_3(a,b,c) ZRMAX_2(a,ZRMAX_2(b,c))
+#define ZRMAX_4(a,b,c,d) ZRMAX_2(ZRMAX_2(a,b),ZRMAX_2(c,d))
 
 #define ZRISPOW2(I) (((I) & ((I) - 1)) == 0)
 #define ZRISPOW2SAFE(I) ((I) > 0 && ZRISPOW2(I))
 
 #define ZRCARRAY_NBOBJ(array) (sizeof(array)/sizeof(*array))
+
 
 #ifdef __GNUC__
 #define ZRMUSTINLINE __attribute__((always_inline))
