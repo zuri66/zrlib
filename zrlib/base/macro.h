ecbcea64f8499a3ca50b494b703b895e9ee63934
zuri <webzuri@gmail.com>
2020-07-18 13:40:21 +0200

    macro.h, add macros
    - ZRCODE, ZRBLOCK
    - ZRCARRAY_CHECKFORCPY, ZRCARRAY_SAFECPY
    - ZRCARRAY_FROMPOINTERS, ZRCARRAY_CPYPOINTERS

--- zrlib/base/macro.h
+++ zrlib/base/macro.h
@@ -6,6 +6,8 @@
 #ifndef ZRMACRO_H
 #define ZRMACRO_H
 
+//#include <zrlib/config.h>
+#include <stdalign.h>
 #include <stddef.h>
 
 typedef int (*zrfcmp)(void *a, void *b);
@@ -14,7 +16,7 @@ typedef int (*zrfucmp)(void *a, void *b, void* data);
 #define ZRTYPE_SIZE_ALIGNMENT(T) sizeof(T), alignof(T)
 #define ZRTYPE_ALIGNMENT_SIZE(T) alignof(T), sizeof(T)
 
-#define ZRSIZE_UNKNOW (~(size_t)0)
+#define ZRSIZE_UNKNOW SIZE_MAX
 #define ZRTOSTRING(V) #V
 
 #define ZRCONCAT(A,B) ZRCONCAT_(A,B)
@@ -47,43 +49,84 @@ typedef int (*zrfucmp)(void *a, void *b, void* data);
 #define ZRMUSTINLINE
 #endif
 
+#define ZRCODE(...) __VA_ARGS__
+
+#define ZRBLOCK(...) \
+do { \
+	__VA_ARGS__ \
+}while(0)
+
+
+#define ZRCARRAY_CHECKFORCPY(offset, nbObj, maxNbCpy, CODE_ERROR, CODE_END) ZRBLOCK( \
+	size_t const _offset = (offset); \
+	size_t const _nbObj = (nbObj); \
+	\
+	if (_offset >= _nbObj) \
+		CODE_ERROR; \
+	\
+	size_t const _nbFromOffset = _nbObj - _offset; \
+	size_t const _maxNbCpy = (maxNbCpy); \
+	size_t const _nb = ZRMIN(_maxNbCpy, _nbFromOffset); \
+	\
+	CODE_END; \
+)
+
+#define ZRCARRAY_SAFECPY(offset, nbObj, maxNbCpy, i, CODE_ERROR, CODE, CODE_END) \
+	ZRCARRAY_CHECKFORCPY(offset, nbObj, maxNbCpy, CODE_ERROR, ZRCODE( \
+		for (size_t i = 0; i < _nb; i++) { \
+			CODE; \
+		} \
+		CODE_END \
+	) \
+)
+
 //(T, T** pointers, TS, TS* source, size_t nbObj)
-#define ZRCARRAY_TOPOINTERS(TP, pointers, TS, source, nbObj) \
-do \
-{ \
+#define ZRCARRAY_TOPOINTERS(TP, pointers, TS, source, nbObj) ZRBLOCK( \
 	size_t _nb = (nbObj); \
 	TP** P = (pointers); \
 	TS* S = (source); \
 	\
 	while (_nb--) \
 		P[_nb] = (TP*)(&S[_nb]); \
-} \
-while(0)
+)
+
+//(TO, TO* offset, TS, TS** source, size_t nbObj)
+#define ZRCARRAY_FROMPOINTERS(TO, offset, TS, source, nbObj) ZRBLOCK( \
+	size_t _nb = (nbObj); \
+	TO* O = (offset); \
+	TS** S = (source); \
+	\
+	while (_nb--) \
+		O[_nb] = *S[_nb]; \
+)
+
+//(TO, TO** offset, TS, TS** source, size_t nbObj)
+#define ZRCARRAY_CPYPOINTERS(TO, offset, TS, source, nbObj) ZRBLOCK( \
+	size_t _nb = (nbObj); \
+	TO** O = (offset); \
+	TS** S = (source); \
+	\
+	while (_nb--) \
+		O[_nb] = (TO*)S[_nb]; \
+)
 
 //(T, T** pointers, T* source, size_t nbObj)
-#define ZRCARRAY_TOPOINTERSDATA(T, pointers, source, nbObj) \
-do \
-{ \
+#define ZRCARRAY_TOPOINTERSDATA(T, pointers, source, nbObj) ZRBLOCK( \
 	size_t _nb = (nbObj); \
 	T** P = (pointers); \
 	T* S = (source); \
 	\
 	while (_nb--) \
 		*P[_nb] = S[_nb]; \
-} \
-while(0)
+)
 
-//(T, T* offset, size_t nbObj, T** source)
-#define ZRCARRAY_FROMPOINTERSDATA(T, offset, source, nbObj) \
-do \
-{ \
+//(T, T* offset, T** source, size_t nbObj)
+#define ZRCARRAY_FROMPOINTERSDATA(T, offset, source, nbObj) ZRBLOCK( \
 	size_t _nb = (nbObj); \
 	T* O = (offset); \
 	T** S = (source); \
 	\
 	while (_nb--) \
 		O[_nb] = *S[_nb]; \
-} \
-while(0)
-
+)
 #endif
