c2c673291700833338586ee3d910bc98c468508c
zuri <webzuri@gmail.com>
2020-10-02 12:24:19 +0200

    Move MPoolDynamicStrategy_help.c in main MPoolDynamicStrategy.c

--- src/base/MemoryPool/MPoolDynamicStrategy.c
+++ src/base/MemoryPool/MPoolDynamicStrategy.c
@@ -307,4 +307,27 @@ void ZRMPoolDS_destroy(ZRMemoryPool *pool)
 
 // ============================================================================
 
-#include "MPoolDynamicStrategy_help.c"
+ZRMemoryPool* ZRMPoolDS_create(size_t initialBucketSize, size_t maxFreeBuckets, size_t objSize, size_t objAlignment, ZRAllocator *allocator)
+{
+	ZRMemoryPoolStrategy *strategy = ZRALLOC(allocator, sizeof(ZRMPoolDynamicStrategy));
+	ZRMPoolDS_init(strategy, allocator, initialBucketSize, maxFreeBuckets);
+	strategy->fdestroy = ZRMPoolDS_destroy;
+	ZRMemoryPool *pool = ZRALLOC(allocator, sizeof(ZRMPoolDS));
+	ZRMPool_init(pool, objSize, objAlignment, strategy);
+	return pool;
+}
+
+ZRMemoryPool* ZRMPoolDS_createBS(size_t initialBucketSize, size_t objSize, size_t objAlignment, ZRAllocator *allocator)
+{
+	return ZRMPoolDS_create(initialBucketSize, DEFAULT_MAX_FREE_BUCKETS, objSize, objAlignment, allocator);
+}
+
+ZRMemoryPool* ZRMPoolDS_createMaxFB(size_t maxFreeBuckets, size_t objSize, size_t objAlignment, ZRAllocator *allocator)
+{
+	return ZRMPoolDS_create(INITIAL_BUCKET_SIZE, maxFreeBuckets, objSize, objAlignment, allocator);
+}
+
+ZRMemoryPool* ZRMPoolDS_createDefault(size_t objSize, size_t objAlignment, ZRAllocator *allocator)
+{
+	return ZRMPoolDS_create(INITIAL_BUCKET_SIZE, DEFAULT_MAX_FREE_BUCKETS, objSize, objAlignment, allocator);
+}
